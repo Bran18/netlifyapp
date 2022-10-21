8fe88e6cfecc2f878bb501c0fc1345ac5e53e53a
Bran18 <andreyfdez18@gmail.com>
2022-10-20 23:27:14 -0600

    authReady

--- stores/authContext.js
+++ stores/authContext.js
@@ -11,19 +11,28 @@ const AuthContext = createContext({
 export const AuthContextProvider = ({children}) => {
 
     const [user, setUser] = useState(null)
+    const [authReady, setAuthReady] = useState(false)
 
     useEffect(() => {
         //event listener from netlify
         netlifyIdentity.on('login', (user) => {
             setUser(user)
+           
             // force the modal to close
             netlifyIdentity.close()
             console.log('login event success')
             //event listener for logout
             netlifyIdentity.on('logout', (user) => {
                 setUser(null)
+               
                 console.log('logout finished')
             })
+        })
+
+        netlifyIdentity.on('init', (user) => {
+            setUser(user)
+            setAuthReady(true)
+            console.log("init event")
         })
         //init netlify identity connection
         netlifyIdentity.init()
@@ -48,7 +57,7 @@ export const AuthContextProvider = ({children}) => {
 
     // context object
     const context = {
-        user,login,logout
+        user,login,logout, authReady
     }
     return (
         <AuthContext.Provider value={context}>
